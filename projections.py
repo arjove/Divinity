2cfe270d2f1d7a038bad891886a43025341ed12e
Tristan de Boer <info@tristandeboer.nl>
2019-10-03 17:00:54 +0200

    Changed GOOGLE_AUTH_TOKEN to GOOGLE_SERVER_AUTH_TOKEN

--- projections.py
+++ projections.py
@@ -7,8 +7,8 @@ from cachetools import cached, LRUCache
 from cachetools.keys import hashkey
 
 #from secrets import GOOGLE_API_KEY
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
-gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+GOOGLE_SERVER_AUTH_TOKEN = os.environ.get('GOOGLE_SERVER_AUTH_TOKEN')
+gmaps = googlemaps.Client(key=GOOGLE_SERVER_AUTH_TOKEN)
 
 AREAS = 'Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot'
 
@@ -84,7 +84,7 @@ def process(socket, request, group_info_param):
 	# Project location for 3 nearest groups
 	for target in range(3):
 			# Create a copy of group_info_param
-			group_info = group_info_param[:] 
+			group_info = group_info_param[:]
 			# Dictionary that contains an entry of following format per group
 			# [waypoint_groups, next group, current_step, step_progress, polylines]
 			option = {}
@@ -118,9 +118,9 @@ def process(socket, request, group_info_param):
 						else:
 							# We are at our destination and there is time left
 							visited[group_info.index(nearest[0])] += 1 # In list of visit numbers, increment visited group
-							
+
 							# Append visisted group to waypoints
-							nearest[0]['visits'] += 1 
+							nearest[0]['visits'] += 1
 							waypoints.append(nearest[0])
 
 							# Increment value in group_info
@@ -137,7 +137,7 @@ def process(socket, request, group_info_param):
 					continue
 
 			projections.append([option])
-			
+
 			print('[+] Estimated current location')
 
 	print('[!] Sending projections')
